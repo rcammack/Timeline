de0e94fbd42a4b54ccb588cce9ad8bef9a9225e1
rcammack <rcammack@hawaii.edu>
2018-10-18 21:20:51 -1000

    dynamically create buttons/popups

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -1,34 +1,11 @@
 //file: timeline.js
 
-// given an array of course objects
-function plotCourses(data) {
-    courses = data[0]['classes'];
-    projects = data[0]['projects'];
-    
-    //initialize min and max times with first time entry
-    let min, max;
-    let time0 = courses[0].time.split('/');
-    const mydate0 = new Date(time0[2], time0[0] - 1, time0[1]);
-    document.getElementById('test').innerHTML = mydate0.toDateString();
-    min = mydate0;
-    max = min;
-    
-    //get array of times to plot
+// given an array of data, plot the timeline
+function plotTimeline(data) {
+    // document.getElementById('header').innerHTML = 'riley\'s timeline';
+    let min, max; //min and max dates to plot
+    //get array of dates to plot
     let timeline = [];
-    // for (let i = 0; i < courses.length; i++) {
-    //     let course = courses[i];
-    //     let time = course.time.split('/');
-    //     let mydate = new Date(time[2], time[0] - 1, time[1]);
-    //     min = mydate < min ? mydate : min;
-    //     max = mydate > max ? mydate : max;
-    //     timeline.push(mydate);
-    // }
-    // for (let i = 0; i < timeline.length; i++) {
-    //     let percent = (timeline[i]-min)/(max-min) * 100; //percentage of time from timeline start to total time
-    //     timeline[i] = percent.toString() + '%';
-    // }
-    // console.log(timeline);
-
     for (let j = 0; j < data.length; j++) {
         elem = data[j];
         for(var propName in elem) {
@@ -38,6 +15,12 @@ function plotCourses(data) {
                     let course = propValue[i];
                     let time = course.time.split('/');
                     let mydate = new Date(time[2], time[0] - 1, time[1]);
+                    if (typeof min === 'undefined'){
+                        min = mydate;
+                    }
+                    if (typeof max === 'undefined'){
+                        max = mydate;
+                    }
                     min = mydate < min ? mydate : min;
                     max = mydate > max ? mydate : max;
                     timeline.push(mydate);
@@ -45,29 +28,56 @@ function plotCourses(data) {
             }
         }
     }
+    //convert dates in timeline array to percentages along timeline
     for (let i = 0; i < timeline.length; i++) {
         let percent = (timeline[i]-min)/(max-min) * 100; //percentage of time from timeline start to total time
         timeline[i] = percent.toString() + '%';
     }
     console.log(timeline);
-    
-    /////////////////////////////////////////
-    let date = courses[0].date;
-    let content = courses[0].content;
-    $('#fall18').popup({
-        title   : date,
-        content : content
-    });
-    $('#fall18')[0].style.top = timeline[1];
-    $('#grad').popup({
-        title   : 'May 2019',
-        content : 'graduation!'
-    });
-    $('#grad')[0].style.top = timeline[2];
 
+////////////////////create buttons/////////////////////////
+    let k = 0;
+    for (let j = 0; j < data.length; j++) {
+        elem = data[j];
+        for(var propName in elem) {
+            if(elem.hasOwnProperty(propName)) {
+                var propValue = elem[propName];
+                for (let i = 0; i < propValue.length; i++) {
+                    createButton(propValue[i], timeline[k]);
+                    k++;
+                }
+            }
+        }
+    }
     return;
 }
 
+function createButton(data, time) {
+    //button
+    var btn = document.createElement("BUTTON");
+    btn.classList.add("circular");
+    btn.classList.add("ui");
+    btn.classList.add("button");
+    btn.classList.add("blue");
+    btn.classList.add("icon");
+    //icon
+    var icon = document.createElement("I");
+    icon.classList.add("icon");
+    icon.classList.add("book");
+    btn.appendChild(icon);
+    //popup
+    $(btn).popup({
+        title   : data.title,
+        content : data.content,
+        position: 'right center'
+    });
+    //position on timeline
+    $(btn)[0].style.position = 'absolute';
+    $(btn)[0].style.top = time;
+    //add to document
+    document.getElementById("timeline").appendChild(btn);
+}
+
 $.getJSON("/../data/data.json", function (data) {
-    plotCourses(data);
+    plotTimeline(data);
 });
